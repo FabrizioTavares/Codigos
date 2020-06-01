6f74c51e55954e8ea5127c093ff19d33bcb818b2
FabrizioTavares <63682778+FabrizioTavares@users.noreply.github.com>
2020-06-01 16:48:52 -0300

    Update graphreader.py
    Change log:
    + Now a function
    + Added functionalities: count vertex and edges, if both arguments are true, prints complexity.

--- graphreader.py
+++ graphreader.py
@@ -1,34 +1,54 @@
-datainput = open("graph.txt", "r")  # replace first argument with wanted graph
 
-graph = []
+def readgraph(file, countvertex=False, countedges=False):
+    datainput = open(file, "r")  # replace first argument with wanted graph
 
-linereader = datainput.readlines()  # Create object so the .readlines() method is only called once
+    graph = []
 
-# DO NOT EDIT THESE LINES!!
-line = int(0)
-indexread = int(0)
-#
+    linereader = datainput.readlines()  # Create object so the .readlines() method is only called once
+    datainput.close()
 
-while True:  # Not ideal. Change ASAP/Find better approach.
-    try:
-        graph.append(linereader[line].split())
-        line += 1
-    except IndexError:
-        break
+    # DO NOT EDIT THESE LINES!!
+    line = int(0)
+    indexread = int(0)
 
-# vertex count = len(graph)
-
-printerList = []  # Auxiliary list to help print the relations
-
-for i in range(0, len(graph)):  # this is to print the relations in a graph list way.
-    while True:  # Use while since the lenght of a relationship is unknown. 'for' can also be used, only if lenght known
-        try:  # run these lines until program finds an exception. Occurs when there's no more relationships.
-            printerList.append(graph[i][indexread])
-            indexread += 1  # this is needed to trigger an exception.
+    # This loop reads each line of the file.
+    while True:  # Not ideal. Change ASAP/Find better approach.
+        try:
+            graph.append(linereader[line].split())
+            line += 1
         except IndexError:
-            print(printerList[0], ": ", end='')  # end='' means no new line appended to string
-            printerList.remove(printerList[0])
-            print(printerList)
-            printerList = []
-            indexread = 0
-            break
+            break  # Stop loop after file runs out of lines
+
+    printer_list = []  # Auxiliary list to help print the relations
+    for i in range(0, len(graph)):  # this is to print the relations in a graph list way.
+        while True:  # Use while since the lenght of a relationship is unknown.
+            try:  # run these lines until program finds an exception. Occurs when there's no more relationships.
+                printer_list.append(graph[i][indexread])
+                indexread += 1  # this is needed to trigger an exception.
+            except IndexError:
+                print(printer_list[0], ": ", end='')  # end='' means no new line appended to string
+                printer_list.remove(printer_list[0])  # Remove index 0 so it is not printed (Avoid self loop)
+                print(printer_list)  # Print only the relationships
+                printer_list = []  # Clear the list to receive new values
+                indexread = 0  # Zero the index to read, since new values will be read.
+                break
+
+    if countvertex:
+        vertexcountlist = []
+        print("WARNING! HIGH COMPLEXITY OPERATION. THIS MAY TAKE A WHILE")
+        for k in range(0, len(graph)):
+            for j in range(0, len(graph[k])):
+                if graph[k][j] not in vertexcountlist:
+                    vertexcountlist.append(graph[k][j])
+        countingvertex = len(vertexcountlist)
+        print("Number of vertex: ", countingvertex)
+
+    if countedges:
+        countingedges = int(0)
+        for m in range(0, len(graph)):
+            countingedges += len(graph[m])
+        countingedges -= len(graph)
+        print("Number of edges:", countingedges)
+
+    if countedges and countvertex:
+        print("Complexity: ( {} + {} )".format(countingvertex, countingedges))
